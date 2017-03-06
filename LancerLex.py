34e4bc98246992b8830fdbe6480a47995dac0bb2
Alan Gustavo Valdez Cascajares <Alan Gustavo Valdez Cascajares>
2017-03-05 19:13:06 -0600

    Finished Lexer.#

--- LancerLex.py
+++ LancerLex.py
@@ -1,4 +1,149 @@
 # Lancer: Created by Alan Valdez & Rafael Manriquez
 
+#Alan Gustavo Valdez Cascajares A01336955
+
 import ply.lex as lex
+
+#Diccionario de palabras reservadas {palabra : TOKEN}
+reserved = {
+    'program': 'PROGRAMA',
+    'If': 'IF',
+    'var': 'VAR',
+    'Else': 'ELSE',
+    'Int': 'INT_TYPE',
+    'Float': 'FLOAT_TYPE',
+    'Print': 'PRINT',
+    'Bool' : 'BOOL_TYPE',
+    'String' : 'STRING_TYPE',
+    'Func' : 'FUNC',
+    'False' : 'FALSE',
+    'True' : 'TRUE',
+    'While' : 'WHILE',
+    'let' : 'CONST',
+    'Main' : 'MAIN',
+    'Return' : 'RETURN',
+    'Void' : 'VOID',
+    'DrawSquare' : 'DRAWSQUARE',
+    'DrawCircle' : 'DRAWCIRCLE',
+    'DrawLine' : 'DRAWLINE',
+    'DrawPolygon' : 'DRAWPOLYGON',
+    'DrawCurve' : 'DRAWCURVE',
+    'Input' : 'INPUT',
+    'DrawTriangle' : 'DRAWTRIANGLE',
+    'Red' : 'RED',
+    'Green' : 'GREEN',
+    'Blue' : 'BLUE',
+    'Yellow' : 'YELLOW',
+    'Brown' : 'BROWN',
+    'Black' : 'BLACK',
+    'ElseIf' : 'ELSEIF'
+}
+
+#Lista de tokens
+tokens = [
+             #	'INTEGER',
+             #	'FLOAT',
+             #	'BINARY',
+             #	'HEX',
+             'ID',
+             'PLUS',
+             'MINUS',
+             'TIMES',
+             'DIVISION',
+             'LPAREN',
+             'RPAREN',
+             'LBRACKET',
+             'RBRACKET',
+             'LARRAY',
+             'RARRAY',
+             'COLON',
+             'SEMICOLON',
+             'GT',
+             'LT',
+             'GE',
+             'LE',
+             'EQUAL',
+             'NOT',
+             'COMA',
+             'AND',
+             'OR',
+             'DIFFERENT',
+             'DOT',
+             'ASSIGN',
+             'STRING',
+             'CTEI',
+             'CTEF'
+         ] + list(reserved.values())
+
+#Expresiones regulares para la definicion de cada token
+t_PLUS = r'\+'
+t_MINUS = r'\-'
+t_TIMES = r'\*'
+t_DIVISION = r'/'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_LBRACKET = r'\{'
+t_RBRACKET = r'\}'
+t_LARRAY = r'\['
+t_RARRAY = r'\]'
+t_GT = r'\>'
+t_LT = r'\<'
+t_GE = r'\>='
+t_LE = r'\<='
+t_NOT = r'\!!'
+t_EQUAL = r'\=='
+t_COLON = r'\:'
+t_SEMICOLON = r'\;'
+t_STRING = r'\".*\"'
+t_COMA = r'\,'
+t_AND = r'\&&'
+t_OR = r'\|\|'
+t_DIFFERENT = r'\<>'
+t_DOT = r'\.'
+t_ASSIGN = r'\='
+
+# t_BINARY = r'[01]+[bB]'
+# t_INTEGER = r'[0-9]+'
+# t_HEX = '((0[A-Fa-f])|([0-9]))[0-9A-Fa-f]*[Hh]'
+
+#Caracteres a ignorar
+t_ignore = ' \t\n'
+
+#Definicion de token ID
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+
+    #Verificar si el ID es una palabra reservada, si es reservada, cambia el tipo de token al correspondiente
+    t.type = reserved.get(t.value, 'ID')  # Check for reserved words
+    return t
+
+def t_CTEF(t):
+    r'[0-9]+\.[0-9][0-9]*'
+    return t
+
+def t_CTEI(t):
+    r'[0-9]+'
+    return t
+
+#Error lexico
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+#Inicializar lex
+lexer = lex.lex()
+
+#Datos para probar el analizador lexico
+data = 'Int'
+
+#Datos como input del analizador lexico
+lexer.input(data)
+
+#Loop que termina al leer todos los caracteres de los datos usados
+while True:
+    tok = lexer.token()
+    if not tok:
+        break
+    print (tok)
+
 
--- LancerYacc.py
+++ LancerYacc.py
@@ -2,3 +2,212 @@
 
 import ply.yacc as yacc
 
+from LancerLex import tokens
+
+#Reglas gramaticales expresadas en funciones
+def p_expression_program(p):
+    'program : PROGRAM ID SEMICOLON s1 s2 BLOCK'
+
+def p_expression_program_s1(p):
+    's1 : VARS | empty'
+
+def p_expression_program_s2(p):
+    '''s2 : FUNCTION s2
+          | empty'''
+
+def p_expression_vars(p):
+    'vars : VAR s1 COLON type SEMICOLON s1'
+
+def p_expression_vars_s1(p):
+    '''s1 : ID
+          | ID COMMA s1
+          | empty'''
+
+def p_expression_type(p):
+    '''type : INT
+            | FLOAT
+            | BOOL
+            | STRING
+            | array'''
+
+def p_expression_array(p):
+    '''array : INT s1
+             | FLOAT s1
+             | STRING s1'''
+
+def p_expression_array_s1(p):
+    's1 : [ CTE-I ]'
+
+def p_expression_block(p):
+    'block : { s1 }'
+
+def p_expression_block_s1(p):
+    '''s1 : statue s1
+          | empty'''
+
+def p_expression_statue(p):
+    '''statue : assignment
+              | condition
+              | read
+              | write
+              | cycle
+              | functioncall
+              | predefined
+              | return'''
+
+def p_expression_parameter(p):
+    '''parameter : empty
+                 | s1'''
+
+def p_expression_parameter_s1(p):
+    '''s1 : type ID
+          | type ID COMMA s1'''
+
+def p_expression_return(p):
+    'return : RETURN ssexpression SEMICOLON'
+
+def p_expression_assignment(p):
+    'assignment : ID s1 = s2'
+
+def p_expression_assignment_s1(p):
+    '''s1 : empty
+          | LBRACKET ssexpression RBRACKET'''
+
+def p_expression_assignment_s2(p):
+    '''s2 : functioncall
+          | ssexpression'''
+
+def p_expression_ssexpression(p):
+    '''ssexpression : sexpression
+                    | !! sexpression'''
+
+def p_expression_sexpression(p):
+    'sexpression : expression s1'
+
+def p_expression_sexpression_s1(p):
+    '''s1 : empty
+          | && sexpression
+          | || sexpression'''
+
+def p_expression_expression(p):
+    'expression : exp s1'
+
+def p_expression_expression_s1(p):
+    '''s1 : empty
+          | GT exp
+          | LT exp
+          | ET exp
+          | NE exp'''
+
+def p_expression_term(p):
+    'term : factor s1'
+
+def p_expression_term_s1(p):
+    '''s1 : empty
+          | TIMES term
+          | DIVISION term'''
+
+def p_expression_exp(p):
+    'exp : term s1'
+
+def p_expression_exp_s1(p):
+    '''s1 : empty
+          | PLUS term
+          | MINUS term'''
+
+def p_expression_predefined(p):
+    '''predefined : drawcircle
+                  | drawsquare
+                  | drawtriangle
+                  | drawline
+                  | drawpolygon
+                  | drawcurve'''
+
+
+def p_expression_factor(p):
+    '''factor : '''
+
+def p_expression_condition(p):
+    'condition : IF s1 RPAREN block s3'
+
+def p_expression_condition_s1(p):
+    '''s1 : LPAREN s2
+          | !! s2'''
+
+def p_expression_condition_s2(p):
+    '''s2 : ssexpression
+          | functioncall'''
+
+def p_expression_condition_s3(p):
+    '''s3 : empty
+          | ELSE block
+          | s1'''
+
+def p_expression_read(p):
+    'read : ID EQUAL INPUT SEMICOLON'
+
+def p_expression_write(p):
+    '''write : PRINT LPAREN ssexpression RPAREN'''
+
+def p_expression_color(p):
+    '''color : RED
+             | GREEN
+             | BLUE
+             | YELLOW
+             | BROWN
+             | BLACK'''
+
+def p_expression_cycle(p):
+    'cycle : WHILE LPAREN ssexpression RPAREN block'
+
+def p_expression_function(p):
+    'function : FUNC s1 ID LPAREN parameter RPAREN block'
+
+def p_expression_function_s1(p):
+    '''s1 : VOID
+          | type'''
+
+def p_expression_functioncall(p):
+    'functioncall : ID LPAREN s1 RPAREN SEMICOLON'
+
+def p_expression_functioncall_s1(p):
+    '''s1 : empty
+          | ssexpression
+          | ssexpression COMMA s1'''
+
+
+''' FALTAN LAS DE DRAW '''
+
+#Error de sintaxis
+def p_error(p):
+    print("Syntax error in input!")
+
+#Definicion de espacio vacio
+def p_empty(p):
+    'empty :'
+    pass
+
+#Construir el parser
+parser = yacc.yacc(debug=1)
+
+#Escribir el nombre o ruta del archivo a leer
+print("Nombre o ruta dek archivo a analizar: ")
+fileName = raw_input()
+
+#Abrir archivo
+file = open(fileName, 'r')
+code = file.read()
+
+#Imprimir codigo leido
+print (code)
+
+#Parsear el codigo leido del archivo
+parser.parse(code)
+# while True:
+#    try:
+#        s = raw_input()
+#    except EOFError:
+#        break
+#    if not s: continue
+#    result = parser.parse(code)
+#    print(result)
